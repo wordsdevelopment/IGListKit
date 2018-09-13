d240ecd2b04a0cc5392d2011aacabf46663d1424
Scott MacGregor <smacgregor@zynga.com>
2018-09-13 09:59:36 -0700

    Fix a compilation error on Xcode 10

--- Source/IGListCollectionView.h
+++ Source/IGListCollectionView.h
@@ -22,10 +22,10 @@ IGLK_SUBCLASSING_RESTRICTED
 /**
  :nodoc:
  */
-- (void)performBatchUpdates:(void (^)(void))updates
+- (void)performBatchUpdates:(__attribute__((noescape)) void (^)(void))updates
                  completion:(void (^)(BOOL))completion IGLK_UNAVAILABLE("Call -[IGListAdapter performUpdatesWithCompletion:] instead");
 
-/**
+/**\
  :nodoc:
  */
 - (void)reloadData IGLK_UNAVAILABLE("Call -[IGListAdapter reloadDataWithCompletion:] instead");
